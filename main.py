1b8cf974ad180c673a99c09447d9439769015f83
juan <jpuerto96@hotmail.com>
2025-03-17 19:21:05 -0400

    General: .01 to .02

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def perform_pick(arm, grasp_pose, lift_pose):
 
 def calculate_pose(fa, count):
     place_pose = RigidTransform(
-            translation = [0.6096255 + count*0.03, 0.1271784, 0.02008005],
+            translation = [0.6096255 + count*0.03, 0.1271784, 0.01008005],
             rotation = [[-0.11705924,  0.99311342,  0.00190074],
             [ 0.9905303,   0.11661634,  0.07232698],
             [ 0.07160724,  0.01034928, -0.99737916]],
@@ -186,8 +186,5 @@ if __name__ == "__main__":
                 place_pose = calculate_pose(fa, count)
                 perform_place(fa, place_pose, T_lift_world)
                 count += 1
-
-            # This is to avoid the Franka doing the same thing each time while we work on perception
-            exit(0)
 
     import IPython; IPython.embed(); exit(0)
