baac7a606b96d41b2ae0e00263167332e90e3cf6
juan <jpuerto96@hotmail.com>
2025-04-08 16:14:55 -0400

    Repeat pick attempts

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def perform_pick(fa, pick_pose, lift_pose, no_gripper):
 
 def calculate_pose(col, row):
     place_pose = RigidTransform(
-        translation=[0.54875245, 0.11862949 + col * 0.05, 0.01705035 + row*0.03],
+        translation=[0.54875245, 0.11862949 + col * 0.06, 0.01705035 + row*0.05],
         rotation=[[-0.02087884, 0.99942336, 0.02641552],
                   [0.99757839, 0.01907633, 0.06674037],
                   [0.06619797, 0.02774502, -0.99742065]],
@@ -71,7 +71,7 @@ def perform_place(fa, place_pose, lift_pose, no_gripper):
     fa.goto_pose(place_pose)
 
     if not no_gripper:
-        fa.goto_gripper(0.05)
+        fa.open_gripper()
 
     fa.goto_pose(lift_pose)
 
@@ -80,6 +80,11 @@ def color_matches(marker_color, target_color, threshold=0.7):
     return (abs(marker_color.r - target_color[0]) < threshold and
             abs(marker_color.g - target_color[1]) < threshold and
             abs(marker_color.b - target_color[2]) < threshold)
+
+def get_block_position(fa, color_to_find):
+    fa.goto_pose(T_observe_pick_world)
+    sleep(1)
+    return get_block_by_color(color_to_find)
 
 
 def get_block_by_color(target_color_name):
@@ -187,34 +192,44 @@ if __name__ == "__main__":
         logging.info(f'Row configuration: {row_configuration}')
         while len(row_configuration) > 0:
             color_to_find = row_configuration.pop()
-
-            fa.goto_pose(T_observe_pick_world)
-            sleep(1)
-            T_block_world = get_block_by_color(color_to_find)
+            T_block_world = get_block_position(fa, color_to_find)
 
             if not T_block_world:
                 logging.error(f"{color_to_find} block not found, exiting.")
                 exit(2)
+
             print(T_block_world)
 
-#            T_block_world.translation[0] += cube_size/2
-#            T_block_world.translation[1] -= cube_size/6
+           # T_block_world.translation[0] += cube_size/2
+           # T_block_world.translation[1] -= cube_size/6
 
             print(T_block_world)
             # Get grasp pose
             T_grasp_world = get_closest_grasp_pose(T_block_world, T_ready_world, cube_size)
             print(T_grasp_world)
-            T_grasp_world.translation[0] += cube_size/2
+            # T_grasp_world.translation[0] += cube_size/2
 
             T_place_world = calculate_pose(col, row)
 
             # Pose closer to pick/place poses
-            T_lift = RigidTransform(translation=[0, 0, 0.05], from_frame=T_ready_world.to_frame,
+            T_lift = RigidTransform(translation=[0, 0, cube_size+0.02], from_frame=T_ready_world.to_frame,
                                     to_frame=T_ready_world.to_frame)
             T_lift_pick_world = T_lift * T_grasp_world
             T_lift_place_world = T_lift * T_place_world
 
-            perform_pick(fa, T_grasp_world, T_lift_pick_world, args.no_grasp)
+            num_pick_attempts = 3
+            pick_failure = True
+            while pick_failure:
+                perform_pick(fa, T_grasp_world, T_lift_pick_world, args.no_grasp)
+                pick_failure = fa.get_gripper_width() < 0.004
+                if num_pick_attempts == 0 or not pick_failure:
+                    break
+
+                num_pick_attempts -= 1
+                T_block_world = get_block_position(fa, color_to_find)
+                T_grasp_world = get_closest_grasp_pose(T_block_world, T_ready_world, cube_size)
+                T_lift_pick_world = T_lift * T_grasp_world
+
             fa.goto_pose(T_ready_world)
             perform_place(fa, T_place_world, T_lift_place_world, args.no_grasp)
             fa.goto_pose(T_ready_world)
